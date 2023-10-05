fceeb852faebaa8e1a412e2060d7e691a50b1b5d
Caio Vieira <caiovieirarodrigues03@gmail.com>
2023-10-05 14:02:01 -0300

    enemy update

--- Assets/Scripts/EnemyKamikaze.cs
+++ Assets/Scripts/EnemyKamikaze.cs
@@ -5,20 +5,46 @@ using UnityEngine;
 public class EnemyKamikaze : MonoBehaviour
 {
     #region Fields
-    //[Header("Settings")]
+    [Header("Settings")]
+    public int speed = 5;
     //[Header("References")]
     //[Header("Debug")]
     #endregion
-    
+
     #region Properties
     #endregion
-    
+
     #region Unity Messages
     #endregion
-    
+
     #region Public Methods
+    void Update()
+    {
+        lookAtPlayer();
+
+    }
     #endregion
-    
+
     #region Private Methods
+    private void lookAtPlayer()
+    {
+        // Encontre o jogador pelo nome da tag
+        GameObject jogador = GameObject.FindWithTag("Player");
+
+        if (jogador != null)
+        {
+            // Faça o objeto olhar para o jogador
+            transform.LookAt(jogador.transform);
+
+            // Mantenha a rotação X e Z em 0
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.LogWarning("Jogador não encontrado com a tag " + "Player");
+        }
+    }
     #endregion
 }
--- Assets/Scripts/EnemyStationary.cs
+++ Assets/Scripts/EnemyStationary.cs
@@ -7,18 +7,42 @@ public class EnemyStationary : MonoBehaviour
     #region Fields
     //[Header("Settings")]
     //[Header("References")]
+
     //[Header("Debug")]
     #endregion
-    
+
     #region Properties
     #endregion
-    
+
     #region Unity Messages
     #endregion
-    
+
     #region Public Methods
+    void Update()
+    {
+        lookAtPlayer();
+
+    }
     #endregion
-    
+
     #region Private Methods
+    private void lookAtPlayer()
+    {
+        // Encontre o jogador pelo nome da tag
+        GameObject jogador = GameObject.FindWithTag("Player");
+
+        if (jogador != null)
+        {
+            // Faça o objeto olhar para o jogador
+            transform.LookAt(jogador.transform);
+
+            // Mantenha a rotação X e Z em 0
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+        else
+        {
+            Debug.LogWarning("Jogador não encontrado com a tag " + "Player");
+        }
+    }
     #endregion
 }
